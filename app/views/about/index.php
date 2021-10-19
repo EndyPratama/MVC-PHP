2d07f960da8d6175fbe418ec1fe00b6240bcca0b
Endy Gigih Pratama <54503473+EndyPratama@users.noreply.github.com>
2021-10-19 23:42:13 +0700

    view

---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1>About index</h1>
+    <img src="<?= BASEURL; ?>/img/me.jpeg" alt="Endy Gigih Pratama" width="200" class="rounded-circle shadow">
+    <p>Hallo, Nama saya <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?>, saya seorang <?= $data['pekerjaan'] ?></p>
+</div>
